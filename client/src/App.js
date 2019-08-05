cf50855e24f367735bfbc10f1aa89cd3dce1b07b
Eric Scott <scottzillaster@gmail.com>
2019-08-05 12:44:27 -0500

    updated to full login logout demo

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 class App extends React.Component {
   state = {
     email:'test@test.com',
-    password:'testtest'
+    password:'testtest',
+    welcomeEmail: ""
   }
 
   handleInput = event => {
@@ -18,17 +19,44 @@ class App extends React.Component {
   handleFormSubmit = event => {
     event.preventDefault()
     const {email,password} = this.state
-    axios.post("/api/login", {email, password}).then(result => {console.log(result.data)})
+    axios.post("/api/login", {email, password})
+      .then(result => {
+        console.log(result.data)
+        this.loadProfileInfo()
+      })
+  }
+  handleFormLogout = event => {
+    event.preventDefault()
+    axios.get("/api/logout")
+      .then(result => {
+        console.log(result.data)
+        this.setState({ welcomeEmail: ""})
+      })
+  }
+
+  loadProfileInfo = () => {
+    axios.get('/api/user/me')
+      .then(response => {
+        this.setState({welcomeEmail: response.data.email})
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount(){
+    this.loadProfileInfo()
   }
 
   render(){
     return (
       <div>
-        <h1>Start</h1>
+        <h1>{this.state.welcomeEmail.length > 0 
+          ? "Welcome " + this.state.welcomeEmail
+          : "Login"}</h1>
         <form>
           <input onChange={this.handleInput} name="email" value={this.state.email} type="text"/>
           <input onChange={this.handleInput} name="password" value={this.state.email} type="password"/>
           <button onClick={this.handleFormSubmit}>Login</button>
+          <button onClick={this.handleFormLogout}>Logout</button>
         </form>
       </div>
     );
